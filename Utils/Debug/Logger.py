3035e106530c99b5f81160097ef785e0443da436
Frank B <fboyd78@gmail.com>
2019-08-14 23:42:16 -0400

    Updates: Fixed ANSI colors   wasn't using colorama but importing module   converted hardcoded ANSI colors to colorama syntax   Updated display of text, Label has color, while msg body does not, seems easier on my eyes, could be because it is late my time

--- Utils/Debug/Logger.py
+++ Utils/Debug/Logger.py
@@ -1,32 +1,17 @@
-from colorama import init
+from colorama import Fore, Back, Style
 from enum import Enum
 
-init()
-
-
-class ANSIColor(Enum):
-    
-    BLACK = 30
-    RED = 31
-    GREEN = 32
-    YELLOW = 33
-    BLUE = 34
-    MAGENTA = 35
-    CYAN = 36
-    WHITE = 37
-    RESET = 0
-    
 
 class DebugColorLevel(Enum):
-    
-    WARNING         = ANSIColor.YELLOW.value
-    INFO            = ANSIColor.WHITE.value
-    DEBUG           = ANSIColor.CYAN.value
-    CRITICAL        = ANSIColor.YELLOW.value
-    ERROR           = ANSIColor.RED.value
-    TEST            = ANSIColor.MAGENTA.value
-    SUCCESS         = ANSIColor.GREEN.value
-    NOTIFICATION    = ANSIColor.BLUE.value
+
+    INFO            = Back.WHITE + Fore.BLACK
+    WARNING         = Fore.YELLOW
+    CRITICAL        = Fore.YELLOW + Style.BRIGHT
+    ERROR           = Fore.RED + Style.BRIGHT
+    SUCCESS         = Fore.GREEN + Style.BRIGHT
+    DEBUG           = Fore.CYAN + Style.BRIGHT
+    TEST            = Fore.MAGENTA + Style.BRIGHT
+    NOTIFICATION    = Fore.BLUE + Style.BRIGHT
 
 
 class Logger(object):
@@ -34,44 +19,44 @@ class Logger(object):
     LABEL = ''
 
     @staticmethod
-    def _colorize_message(color: int, msg: str):
-        msg_format = u'\u001b[1m[{label}]: \u001b[1;{msg_color_code}m{msg}\u001b[{reset_color}m'
-        return msg_format.format(label=Logger.LABEL, 
-                msg_color_code=color, 
-                msg=msg, 
-                reset_color=ANSIColor.RESET.value)
+    def _colorize_message(color: str, msg: str):
+        msg_format =  color + '{label}{reset_color}  {msg}'
+        return msg_format.format(label=Logger.LABEL,
+                msg_color_code=color,
+                msg=msg,
+                reset_color=Style.RESET_ALL)
 
     @staticmethod
     def debug(msg: str):
-        Logger.LABEL = 'DEBUG'
+        Logger.LABEL = '[DEBUG]'
         print(Logger._colorize_message(DebugColorLevel.DEBUG.value, msg))
 
     @staticmethod
     def warning(msg: str):
-        Logger.LABEL = 'WARNING'
+        Logger.LABEL = '[WARNING]'
         print(Logger._colorize_message(DebugColorLevel.WARNING.value, msg))
-    
+
     @staticmethod
     def error(msg: str):
-        Logger.LABEL = 'ERROR'
+        Logger.LABEL = '[ERROR]'
         print(Logger._colorize_message(DebugColorLevel.ERROR.value, msg))
 
     @staticmethod
     def info(msg: str):
-        Logger.LABEL = 'INFO'
+        Logger.LABEL = '[INFO]'
         print(Logger._colorize_message(DebugColorLevel.INFO.value, msg))
 
     @staticmethod
     def test(msg: str):
-        Logger.LABEL = 'TEST'
+        Logger.LABEL = '[TEST]'
         print(Logger._colorize_message(DebugColorLevel.TEST.value, msg))
 
     @staticmethod
     def success(msg: str):
-        Logger.LABEL = 'SUCCESS'
+        Logger.LABEL = '[SUCCESS]'
         print(Logger._colorize_message(DebugColorLevel.SUCCESS.value, msg))
 
     @staticmethod
     def notify(msg: str):
-        Logger.LABEL = 'NOTIFICATION'
+        Logger.LABEL = '[NOTIFICATION]'
         print(Logger._colorize_message(DebugColorLevel.NOTIFICATION.value, msg))
