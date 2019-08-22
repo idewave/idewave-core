4f02a1742ce49634626611771b7e9c6a2f989455
Sergio Ivanuzzo <sergio.ivanuzzo@gmail.com>
2019-08-22 15:12:53 +0300

    added missed file

---
+++ Realm/Constants/RealmTimeZone.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+
+class RealmTimezone(Enum):
+
+    DEVELOPMENT     = 1
+    UNITED_STATES   = 2
+    OCEANIC         = 3
+    LATIN_AMERICA   = 4
+    TOURNAMENT      = 5
+    RUSSIAN         = 12
+    TAIWAN          = 14
+    CHINA           = 16
+    TEST_SERVER     = 26
+    QA_SERVER       = 28
